df43761ff205fed304055542a6b29346eb011f1e
Fisher Liang <swimnose@gmail.com>
2016-05-08 10:34:19 +0800

    Parsing data

---
+++ pokedex-by-fisher/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  pokedex-by-fisher
+//
+//  Created by Fisher on 5/8/16.
+//  Copyright © 2016 Xiao Ding, Liang. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
